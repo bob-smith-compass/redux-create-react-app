f73f1a278f57fd89bc50a4a4afb395bd30fdf547
David <david.shams@gmail.com>
2019-08-09 14:06:16 -0500

    Warning: Use the `defaultValue` or `value` props on <select> instead of setting `selected` on <option>

--- src/components/textarea/TextArea.jsx
+++ src/components/textarea/TextArea.jsx
@@ -1,35 +1,43 @@
-import React from 'react';
+import React from "react";
 
 export default class TextArea extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: 'Please write an essay about your favorite DOM element.'
-      };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      event.preventDefault();
-      console.log(this.state);
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Essay:
-            <textarea value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "Please write an essay about your favorite DOM element."
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Essay:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <select>
+          <option value="grapefruit">Grapefruit</option>
+          <option value="lime">Lime</option>
+          <option selected value="coconut">
+            Coconut
+          </option>
+          <option value="mango">Mango</option>
+        </select>
+
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
