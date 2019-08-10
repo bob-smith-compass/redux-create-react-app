0387f7fd23464fffd366099282d499978efa3346
David <david.shams@gmail.com>
2019-08-10 12:02:56 -0500

    setState

--- src/components/conversion/Conversion.jsx
+++ src/components/conversion/Conversion.jsx
@@ -4,10 +4,18 @@ import devounce from 'lodash/debounce';
 import Ddl from '../ddl/Ddl';
 
 export default class Conversion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fromCurrency: '',
+            toCurrency: ''
+        }
+    }
     render() {
         return (
             <div>
-                <Ddl />
+                <Ddl options={['EUR', 'USD', 'CDN']}/>
+                <Ddl options={['EUR', 'USD', 'CDN']}/>
             </div>
         )
     }
--- src/components/ddl/Ddl.jsx
+++ src/components/ddl/Ddl.jsx
@@ -3,17 +3,29 @@ import React, { Component } from 'react'
 export default class Ddl extends Component {
     constructor(props) {
         super(props);
+        console.log(props)
         this.state = {
             selected: '',
-            options: ['Apple', 'Orange', 'Banana']
+            options: this.props.option
         }
+    }
+    changeHandler = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            selected: e.target.value
+        })
+    }
+    showState = () => {
+        console.log(this.state);
     }
     render() {
         return (
             <div>
-                <select name="ddl" id="">
-                    {this.state.options.map( e => <option>{e}</option>)}
+                <select name="ddl" id="" onChange={this.changeHandler}>
+                    {/* {this.state.options.map( e => <option>{e}</option>)} */}
+                    {this.props.options.map( e => <option>{e}</option>)}
                 </select>
+                <button onClick={this.showState}>State</button>
             </div>
         )
     }
