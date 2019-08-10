725bbcf38396a383198b0bc3407ce1fec0737531
David <david.shams@gmail.com>
2019-08-10 13:10:22 -0500

    Uncaught Error: Actions must be plain objects. Use custom middleware for async actions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import Form from './components/form/Forms';
 import TextArea from './components/textarea/TextArea';
 import Conversion from './components/conversion/Conversion';
 
+
+
+import { createStore } from 'redux';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import debounce from 'lodash/debounce';
+
+let defaultState = 0; // does not have to be an object
+
+function reducer(amount, state) {
+    return state + 1;
+}
+
+let store = createStore(reducer);
+
+/**
+ * update state/store
+ * dispatch
+ */
+store.dispatch()
+
 function App() {
   return (
     <div className="App">
--- src/components/redux-conversion/RConversion.jsx
+++ src/components/redux-conversion/RConversion.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import { creatStore } from 'redux';
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import debounce from 'lodash/debounce';
+import React from "react";
+import { createStore } from "redux";
+import PropTypes from "prop-types";
+import axios from "axios";
+import debounce from "lodash/debounce";
 
 let defaultState = 0; // does not have to be an object
 
 function reducer(amount, state) {
-    return state + 1;
+  return state + 1;
 }
 
-let store = creatStore(reducer);
+let store = createStore(reducer);
+
+/**
+ * update state/store
+ * dispatch
+ */
+store.dispatch();
