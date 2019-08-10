5ebeade0e3ebfd315d2b03fbd9ae3fb387a7bf09
David <david.shams@gmail.com>
2019-08-10 13:04:49 -0500

    defaultState reducer creatStore

--- src/components/redux-conversion/RConversion.jsx
+++ src/components/redux-conversion/RConversion.jsx
@@ -3,3 +3,11 @@ import { creatStore } from 'redux';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import debounce from 'lodash/debounce';
+
+let defaultState = 0; // does not have to be an object
+
+function reducer(amount, state) {
+    return state + 1;
+}
+
+let store = creatStore(reducer);
