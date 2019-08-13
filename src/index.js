34bccc33191a58906312a0f469dbdf69b9a991b2
David <david.shams@gmail.com>
2019-08-12 22:47:53 -0500

    Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
+import $ from 'jquery'; // Bootstrap
+import Popper from 'popper.js'; // Bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import React from 'react';
